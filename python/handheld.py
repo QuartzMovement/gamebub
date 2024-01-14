6dd1121ca72324ceda30e1e40f13e8cd996495cb
Eli Lipsitz <eli.lipsitz@gmail.com>
2024-01-14 11:27:36 -0500

    Improve DAC driver

--- python/handheld.py
+++ python/handheld.py
@@ -249,16 +249,24 @@ class TLV320DAC3101:
         self._pin_rst.value(1)
         time.sleep_ms(1)
 
-    def _read_reg(self, reg) -> int:
+    def _set_page(self, page) -> None:
+        self._i2c.writeto_mem(self.i2c_address, 0, bytes([page & 0xFF]))
+
+    def _read_reg(self, page, reg) -> int:
         """Read 8-bit register"""
+        self._set_page(page)
         return self._i2c.readfrom_mem(self.i2c_address, reg, 1)[0]
 
-    def _write_reg(self, reg, data) -> int:
+    def _write_reg(self, page, reg, data) -> int:
         """Write 8-bit register"""
+        self._set_page(page)
         self._i2c.writeto_mem(self.i2c_address, reg, bytes([data & 0xFF]))
 
     def setup(self) -> None:
         """
+        Sets up the DAC with both channels muted, and speakers and headphones
+        both disabled.
+
         # DAC Configuration
 
         ## Filter Selection
@@ -295,119 +303,132 @@ class TLV320DAC3101:
             The options are 1.35 V, 1.5 V, 1.65 V, or 1.8 V, and it must be
             <= AVDD/2. 
             We'll go with 1.5V
-
-
         """
 
         # 1. Set up device.
         self.reset()
 
-        # Set register page to 0
-        self._write_reg(0x00, 0x00)
-
         # Do software reset?
-        # self._write_reg(0x01, 0x01)
+        # self._write_reg(0,  0x01, 0x01)
 
         # 2. Program clock settings
         # PLL_clkin = MCLK, codec_clkin=MCLK
-        self._write_reg(0x04, 0x00)
+        self._write_reg(0, 0x04, 0x00)
 
         # PLL is unused
-        # self._write_reg(0x06, 0x08)
-        # self._write_reg(0x07, 0x00)
-        # self._write_reg(0x08, 0x00)
-        # self._write_reg(0x05, 0x91)
+        # self._write_reg(0, 0x06, 0x08)
+        # self._write_reg(0, 0x07, 0x00)
+        # self._write_reg(0, 0x08, 0x00)
+        # self._write_reg(0, 0x05, 0x91)
 
         # Program and power up NDAC ( = 1)
-        self._write_reg(0x0B, 0x81)
+        self._write_reg(0, 0x0B, 0x81)
 
         # Program and power up MDAC ( = 2)
-        self._write_reg(0x0C, 0x82)
+        self._write_reg(0, 0x0C, 0x82)
 
         # Program OSR
         #
         # DOSR = 128, DOSR(9:8) = 0, DOSR(7:0) = 128
-        self._write_reg(0x0D, 0x00)
-        self._write_reg(0x0E, 0x80)
+        self._write_reg(0, 0x0D, 0x00)
+        self._write_reg(0, 0x0E, 0x80)
 
         # Program codec interface (I2S, 16-bit, BCLK/WCLK inputs)
-        self._write_reg(0x1B, 0x00)
+        self._write_reg(0, 0x1B, 0x00)
 
         # Program processing block. Select PRB_P7
-        self._write_reg(0x3C, 0x07)
+        self._write_reg(0, 0x3C, 0x07)
         # Enable adaptive filtering
-        self._write_reg(0x00, 0x08)
-        self._write_reg(0x01, 0x04)
-        self._write_reg(0x00, 0x00)
+        self._write_reg(0, 0x00, 0x08)
+        self._write_reg(0, 0x01, 0x04)
+        self._write_reg(0, 0x00, 0x00)
 
         # DAC volume control through register, not pin
-        self._write_reg(0x74, 0x00)
+        self._write_reg(0, 0x74, 0x00)
 
         # 3. Program analog blocks
-        # Set register page to 1
-        self._write_reg(0x00, 0x01)
 
         # Program common-mode voltage (set to 1.5 V)
-        self._write_reg(0x1F, 0x0C)
+        self._write_reg(1, 0x1F, 0x0C)
 
         # Program headphone depop settings (power on = 800ms, step = 4ms)
-        self._write_reg(0x21, 0x4E)
+        self._write_reg(1, 0x21, 0x4E)
 
         # Route DAC output to output amplifier mixer
         # LDAC to HPL, RDAC to HPR
-        self._write_reg(0x23, 0x44)
+        self._write_reg(1, 0x23, 0x44)
 
         # Unmute and set gain of output driver
         # Unmute HPL, set gain = 0 db
-        self._write_reg(0x28, 0x06)
+        self._write_reg(1, 0x28, 0x06)
         # Unmute HPR, set gain = 0 dB
-        self._write_reg(0x29, 0x06)
+        self._write_reg(1, 0x29, 0x06)
         # Unmute left speaker, set gain = 6 dB
-        self._write_reg(0x2A, 0x04)
+        self._write_reg(1, 0x2A, 0x04)
         # Unmute right speaker, set gain = 6 dB
-        self._write_reg(0x2B, 0x04)
+        self._write_reg(1, 0x2B, 0x04)
 
-
-        # Power up output drivers
-        # HPL and HPR powered up
-        self._write_reg(0x1F, 0xC2)
-        # Power-up speaker amplifier (left and right)
-        self._write_reg(0x20, 0xC6)
+        # Configure output drivers
         # Enable HPL output analog volume, set = -9 dB
-        self._write_reg(0x24, 0x92)
+        self._write_reg(1, 0x24, 0x92)
         # Enable HPR output analog volume, set = -9 dB
-        self._write_reg(0x25, 0x92)
+        self._write_reg(1, 0x25, 0x92)
         # Enable speaker left output analog volume, set = -9 dB
-        self._write_reg(0x26, 0x92)
+        self._write_reg(1, 0x26, 0x92)
         # Enable speaker right output analog volume, set = -9 dB
-        self._write_reg(0x27, 0x92)
+        self._write_reg(1, 0x27, 0x92)
 
         # TODO: Apply waiting time determined by the de-pop settings and the soft-stepping settings
         #    of the driver gain or poll page 1 / register 63
         # ... 
 
         # 5. Power up DAC
-        # Set register page to 0
-        self._write_reg(0x00, 0x00)
 
         # Powerup DAC left and right channels (soft step enabled)
-        self._write_reg(0x3F, 0xD4)
-
-        # DAC Left gain = -50 dB
-        self._write_reg(0x41, 0b1011_1010)
-        # DAC Right gain = -50 dB
-        self._write_reg(0x42, 0b1011_1010)
-
-        # Unmute DAC left and right channels
-        self._write_reg(0x40, 0x00)
+        self._write_reg(0, 0x3F, 0xD4)
 
         # Enable headphone detection
-        self._write_reg(0x43, 0x80)
-        # (handheld.dac._read_reg(0x43) >> 5) & 0b11
+        self._write_reg(0, 0x43, 0x80)
+
+    def set_volume(self, volume: int) -> None:
+        """
+        Sets DAC volume (range 0 to 255) for left and right.
+        Mapped to DAC's volume range of -63.5 dB to 24 dB.
+        """
+        # map 0 -> -127, 255 -> 48
+        # range = 175
+        value = ((volume * 175) // 255) - 127
+        self._write_reg(0, 0x41, value & 0xFF)
+        self._write_reg(0, 0x42, value & 0xFF)
+
+    def set_mute(self, mute: bool) -> None:
+        # Left and right are individually controllable, but this sets them together.
+        self._write_reg(0, 0x40, 0xC if mute else 0x0)
+
+    def set_headphones_enabled(self, enabled: bool) -> None:
+        if enabled:
+            self._write_reg(1, 0x1F, 0xC2)
+        else:
+            self._write_reg(1, 0x1F, 0x02)
+
+    def set_speakers_enabled(self, enabled: bool) -> None:
+        if enabled:
+            self._write_reg(1, 0x20, 0xC6)
+        else:
+            self._write_reg(1, 0x20, 0x06)
+
+    def get_headphones_detected(self) -> bool:
+        status = (self._read_reg(0, 0x43) >> 5) & 0b11
+        return (status == 0b01) or (status == 0b11)
+
 
 print("Setting up DAC")
 dac = TLV320DAC3101(i2c, dac_reset)
 dac.setup()
+dac.set_volume(64)
+dac.set_mute(False)
+dac.set_headphones_enabled(True)
+dac.set_speakers_enabled(True)
 
 
 
