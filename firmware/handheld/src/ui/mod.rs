cdd689a013303768b136f86d47bc2312c6e558ba
Eli Lipsitz <eli.lipsitz@gmail.com>
2024-03-26 23:47:43 -0400

    Connect UI to device logic

--- firmware/handheld/src/ui/mod.rs
+++ firmware/handheld/src/ui/mod.rs
@@ -1,16 +1,19 @@
 pub mod buttons;
 mod slint;
 
-use std::{rc::Rc, sync::mpsc::Receiver, time::Instant};
+use std::{cell::RefCell, path::Path, rc::Rc, sync::mpsc::Receiver, time::Instant};
 
 pub use buttons::{Button, ButtonEvent};
 
 use ::slint::{
     platform::software_renderer::{MinimalSoftwareWindow, RepaintBufferType, TargetPixel},
-    ComponentHandle, PhysicalSize,
+    ComponentHandle, Model, ModelRc, PhysicalSize, VecModel,
 };
 
-use crate::device::{self, Device, Event};
+use crate::{
+    device::{self, Device, Event},
+    gameboy::Gameboy,
+};
 
 use self::slint::Argb1555;
 
@@ -21,7 +24,7 @@ pub struct UI {
     framebuffer: Vec<Argb1555>,
     window: Rc<MinimalSoftwareWindow>,
     event_queue: Receiver<Event>,
-    _inner: slint::MainWindow,
+    root: slint::MainWindow,
 }
 
 impl UI {
@@ -33,27 +36,22 @@ impl UI {
             window: window.clone(),
         }))
         .unwrap();
-
-        let ui = slint::MainWindow::new().unwrap();
-        ui.global::<slint::Global>().set_battery_level(
-            device
-                .fuel_gauge
-                .get_battery_level()
-                .map_or(0, |x| x.round() as i32),
-        );
-
         window.set_size(PhysicalSize::new(
             DISPLAY_WIDTH as u32,
             DISPLAY_HEIGHT as u32,
         ));
 
+        let root = slint::MainWindow::new().unwrap();
         let event_queue = device.take_event_receiver().unwrap();
-        UI {
+
+        let ui = UI {
             framebuffer,
             window,
             event_queue,
-            _inner: ui,
-        }
+            root,
+        };
+        ui.configure_root(device);
+        ui
     }
 
     pub fn run(&mut self) -> ! {
@@ -119,5 +117,89 @@ impl UI {
                 }
             }
         }
+    }
+
+    fn rom_select_get_files(path: &Path) -> std::io::Result<Vec<String>> {
+        let mut files = path
+            .read_dir()?
+            .filter_map(|e| e.ok())
+            .filter(|f| f.metadata().is_ok_and(|m| m.is_file()))
+            .filter(|f| {
+                f.file_name().to_str().is_some_and(|n| {
+                    !n.starts_with(".") && (n.ends_with(".gbc") || n.ends_with(".gb"))
+                })
+            })
+            .filter_map(|f| f.file_name().into_string().ok())
+            .collect::<Vec<_>>();
+        files.sort();
+        Ok(files)
+    }
+
+    fn configure_root(&self, device: &mut Device) {
+        let backend = self.root.global::<slint::Backend>();
+        let gameboy_ = Rc::new(RefCell::new(Gameboy::new()));
+
+        self.root.global::<slint::Global>().set_battery_level(
+            device
+                .fuel_gauge
+                .get_battery_level()
+                .map_or(0, |x| x.round() as i32),
+        );
+
+        let gameboy = gameboy_.clone();
+        backend.on_main_menu_run_cartridge(move || {
+            gameboy.borrow_mut().set_physical_cartridge().unwrap();
+        });
+
+        let rom_select_path: &Path = "/sdcard/roms".as_ref();
+        let root = self.root.as_weak();
+        backend.on_main_menu_load_rom(move || {
+            let files = Self::rom_select_get_files(rom_select_path).unwrap();
+            let files = ModelRc::from(Rc::new(VecModel::from(
+                files.iter().map(|s| s.into()).collect::<Vec<_>>(),
+            )));
+            root.unwrap()
+                .global::<slint::Global>()
+                .set_rom_select_list(files);
+        });
+
+        let root = self.root.as_weak();
+        let gameboy = gameboy_.clone();
+        backend.on_rom_select_selected(move |index| {
+            let list = root
+                .unwrap()
+                .global::<slint::Global>()
+                .get_rom_select_list();
+            if let Some(data) = list.row_data(index as usize) {
+                let path = rom_select_path.join(data.as_str());
+                log::info!("Selected ROM {}", path.display());
+
+                match gameboy.borrow_mut().set_emulated_cartridge(path.as_path()) {
+                    Ok(_) => true,
+                    Err(e) => {
+                        // TODO show an error message
+                        log::error!("Error loading ROM: {:?}", e);
+                        false
+                    }
+                }
+            } else {
+                false
+            }
+        });
+
+        let gameboy = gameboy_.clone();
+        backend.on_game_set_paused(move |paused| {
+            let mut gameboy = gameboy.borrow_mut();
+            gameboy.set_paused(paused).unwrap();
+            if paused {
+                // TODO handle error more gracefully
+                gameboy.persist_ram().unwrap();
+            }
+        });
+
+        let gameboy = gameboy_.clone();
+        backend.on_game_reset(move || {
+            gameboy.borrow_mut().reset().unwrap();
+        });
     }
 }
