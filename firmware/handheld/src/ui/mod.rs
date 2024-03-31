5fc04bcad7157f7a73a3aa0324f8b457edba27c3
Eli Lipsitz <eli.lipsitz@gmail.com>
2024-03-30 22:21:41 -0400

    Refactor most UI logic to UiState

--- firmware/handheld/src/ui/mod.rs
+++ firmware/handheld/src/ui/mod.rs
@@ -1,13 +1,8 @@
 pub mod buttons;
 mod slint;
+mod state;
 
-use std::{
-    cell::RefCell,
-    path::Path,
-    rc::Rc,
-    sync::mpsc::Receiver,
-    time::{Duration, Instant},
-};
+use std::{cell::RefCell, rc::Rc, sync::mpsc::Receiver, time::Instant};
 
 pub use buttons::{Button, ButtonEvent};
 
@@ -15,16 +10,12 @@ use ::slint::{
     platform::software_renderer::{
         LineBufferProvider, MinimalSoftwareWindow, RepaintBufferType, TargetPixel,
     },
-    platform::WindowAdapter,
-    ComponentHandle, Model, ModelRc, PhysicalSize, Timer, TimerMode, VecModel,
+    PhysicalSize, Timer,
 };
 
-use crate::{
-    device::{self, kvs, Device, Event},
-    gameboy::Gameboy,
-};
+use crate::device::{self, kvs, Device, Event};
 
-use self::slint::Argb1555;
+use self::{slint::Argb1555, state::UiState};
 
 const DISPLAY_WIDTH: usize = 240;
 const DISPLAY_HEIGHT: usize = 160;
@@ -55,11 +46,13 @@ impl<'a, 'b, 'c> LineBufferProvider for &mut LineRenderer<'a, 'b, 'c> {
     }
 }
 
+#[allow(unused)]
 pub struct UI {
     framebuffer: Vec<Argb1555>,
     window: Rc<MinimalSoftwareWindow>,
     event_queue: Receiver<Event>,
     root: slint::MainWindow,
+    state: Rc<RefCell<UiState>>,
 }
 
 impl UI {
@@ -83,9 +76,9 @@ impl UI {
             framebuffer,
             window,
             event_queue,
+            state: UiState::new(&root, device),
             root,
         };
-        ui.configure_root(device);
         ui
     }
 
@@ -158,181 +151,5 @@ impl UI {
                 }
             }
         }
-    }
-
-    fn rom_select_get_files(path: &Path) -> std::io::Result<Vec<String>> {
-        let mut files = path
-            .read_dir()?
-            .filter_map(|e| e.ok())
-            .filter(|f| f.metadata().is_ok_and(|m| m.is_file()))
-            .filter(|f| {
-                f.file_name().to_str().is_some_and(|n| {
-                    !n.starts_with(".") && (n.ends_with(".gbc") || n.ends_with(".gb"))
-                })
-            })
-            .filter_map(|f| f.file_name().into_string().ok())
-            .collect::<Vec<_>>();
-        files.sort();
-        Ok(files)
-    }
-
-    fn configure_root(&self, device: &mut Device) {
-        let backend = self.root.global::<slint::Backend>();
-        let gameboy_ = Rc::new(RefCell::new(Gameboy::new()));
-
-        backend.set_battery_level(
-            device
-                .fuel_gauge
-                .get_battery_level()
-                .map_or(0, |x| x.round() as i32),
-        );
-        backend.set_volume_level(((kvs::keys::VOLUME.get().unwrap() as i32) * 100) / 255);
-        backend.set_brightness_level((kvs::keys::BRIGHTNESS.get().unwrap() * 100.0) as i32);
-
-        let gameboy = gameboy_.clone();
-        backend.on_main_menu_run_cartridge(move || {
-            gameboy.borrow_mut().set_physical_cartridge().unwrap();
-        });
-
-        let rom_select_path: &Path = "/sdcard/roms".as_ref();
-        let root = self.root.as_weak();
-        backend.on_main_menu_load_rom(move || {
-            let last_rom_select = kvs::keys::LAST_ROM_PATH.get();
-            log::info!("last rom select path: {:?}", last_rom_select);
-
-            let files = Self::rom_select_get_files(rom_select_path).unwrap();
-            let files = ModelRc::from(Rc::new(VecModel::from(
-                files.iter().map(|s| s.into()).collect::<Vec<_>>(),
-            )));
-            root.unwrap()
-                .global::<slint::Backend>()
-                .set_rom_select_list(files);
-        });
-
-        let root = self.root.as_weak();
-        let gameboy = gameboy_.clone();
-        backend.on_rom_select_selected(move |index| {
-            let list = root
-                .unwrap()
-                .global::<slint::Backend>()
-                .get_rom_select_list();
-            if let Some(data) = list.row_data(index as usize) {
-                let path = rom_select_path.join(data.as_str());
-                log::info!("Selected ROM {}", path.display());
-                kvs::keys::LAST_ROM_PATH.set(&path);
-
-                match gameboy.borrow_mut().set_emulated_cartridge(path.as_path()) {
-                    Ok(_) => true,
-                    Err(e) => {
-                        // TODO show an error message
-                        log::error!("Error loading ROM: {:?}", e);
-                        false
-                    }
-                }
-            } else {
-                false
-            }
-        });
-
-        let gameboy = gameboy_.clone();
-        backend.on_game_set_paused(move |paused| {
-            let mut gameboy = gameboy.borrow_mut();
-            gameboy.set_paused(paused).unwrap();
-            if paused {
-                // TODO handle error more gracefully
-                gameboy.persist_ram().unwrap();
-            }
-        });
-
-        let gameboy = gameboy_.clone();
-        backend.on_game_reset(move || {
-            gameboy.borrow_mut().reset().unwrap();
-        });
-
-        backend.on_screen_enter(|screen| {
-            // Called when a new screen is entered, before the new frame is rendered.
-            log::info!("Screen enter: {:?}", screen);
-        });
-
-        backend.on_volume_changed({
-            let root = self.root.as_weak();
-            let timer = Timer::default();
-            move |value| {
-                let volume = ((value * (u8::MAX as i32)) / 100) as u8;
-                Device::lock().dac.set_volume(volume).unwrap();
-                // Start the timer to hide the bar.
-                let root = root.clone();
-                timer.start(
-                    TimerMode::SingleShot,
-                    Duration::from_millis(1000),
-                    move || {
-                        kvs::keys::VOLUME.set(&volume);
-                        root.unwrap()
-                            .global::<slint::Backend>()
-                            .set_volume_visible(false);
-                    },
-                )
-            }
-        });
-
-        backend.on_brightness_changed({
-            let root = self.root.as_weak();
-            let timer = Timer::default();
-            move |value| {
-                let brightness = (value as f32) / 100.0;
-                Device::lock().set_brightness(brightness);
-                // Start the timer to hide the bar.
-                let root = root.clone();
-                timer.start(
-                    TimerMode::SingleShot,
-                    Duration::from_millis(1000),
-                    move || {
-                        kvs::keys::BRIGHTNESS.set(&brightness);
-                        root.unwrap()
-                            .global::<slint::Backend>()
-                            .set_brightness_visible(false);
-                    },
-                )
-            }
-        });
-
-        backend.on_power_off(|| {
-            Device::lock().power_off();
-        });
-
-        backend.on_settings_updated(|a, b| {
-            log::info!("settings updated: {:?}, {:?}", a, b);
-        });
-
-        // Focus stack: allowing dialogs to push and pop focus.
-        // Uses private, unstable APIs.
-        let focus_stack = Rc::new(RefCell::new(Vec::new()));
-        backend.on_push_focus({
-            let window = self.window.clone();
-            let stack = focus_stack.clone();
-            move || {
-                let window_inner = ::slint::private_unstable_api::re_exports::WindowInner::from_pub(
-                    window.window(),
-                );
-                let item = window_inner.focus_item.borrow();
-                stack.borrow_mut().push(item.clone());
-            }
-        });
-        backend.on_pop_focus({
-            let window = self.window.clone();
-            let stack = focus_stack.clone();
-            move || {
-                let window_inner = ::slint::private_unstable_api::re_exports::WindowInner::from_pub(
-                    window.window(),
-                );
-                let mut stack = stack.borrow_mut();
-                while let Some(item) = stack.pop() {
-                    if let Some(item) = item.upgrade() {
-                        window_inner.set_focus_item(&item);
-                        break;
-                    }
-                }
-            }
-        });
     }
 }
---
+++ firmware/handheld/src/ui/state.rs
@@ -0,0 +1,193 @@
+use std::{cell::RefCell, path::Path, rc::Rc, time::Duration};
+
+use slint::private_unstable_api::re_exports as slint_re_exports;
+use slint::{ComponentHandle, Global, Model, ModelRc, Timer, TimerMode, VecModel, Weak};
+
+use crate::{
+    device::{kvs, Device},
+    gameboy::Gameboy,
+};
+
+use super::slint::{Backend, MainWindow};
+
+pub struct UiState {
+    root: Weak<MainWindow>,
+    focus_stack: Vec<slint_re_exports::ItemWeak>,
+    gameboy: Gameboy,
+}
+
+impl UiState {
+    pub fn new(root: &MainWindow, device: &mut Device) -> Rc<RefCell<Self>> {
+        let state = UiState {
+            root: root.as_weak(),
+            focus_stack: Vec::new(),
+            gameboy: Gameboy::new(),
+        };
+        let state = Rc::new(RefCell::new(state));
+        state.borrow_mut().setup(state.clone(), device);
+        state
+    }
+
+    fn rom_select_get_files(path: &Path) -> std::io::Result<Vec<String>> {
+        let mut files = path
+            .read_dir()?
+            .filter_map(|e| e.ok())
+            .filter(|f| f.metadata().is_ok_and(|m| m.is_file()))
+            .filter(|f| {
+                f.file_name().to_str().is_some_and(|n| {
+                    !n.starts_with(".") && (n.ends_with(".gbc") || n.ends_with(".gb"))
+                })
+            })
+            .filter_map(|f| f.file_name().into_string().ok())
+            .collect::<Vec<_>>();
+        files.sort();
+        Ok(files)
+    }
+
+    fn setup(&mut self, state: Rc<RefCell<UiState>>, device: &mut Device) {
+        let root = self.root.unwrap();
+        let backend = root.global::<Backend>();
+
+        backend.set_battery_level(
+            device
+                .fuel_gauge
+                .get_battery_level()
+                .map_or(0, |x| x.round() as i32),
+        );
+        backend.set_volume_level(((kvs::keys::VOLUME.get().unwrap() as i32) * 100) / 255);
+        backend.set_brightness_level((kvs::keys::BRIGHTNESS.get().unwrap() * 100.0) as i32);
+
+        let state_ = state.clone();
+        backend.on_main_menu_run_cartridge(move || {
+            state_
+                .borrow_mut()
+                .gameboy
+                .set_physical_cartridge()
+                .unwrap();
+        });
+
+        let rom_select_path: &Path = "/sdcard/roms".as_ref();
+        let state_ = state.clone();
+        backend.on_main_menu_load_rom(move || {
+            let last_rom_select = kvs::keys::LAST_ROM_PATH.get();
+            log::info!("last rom select path: {:?}", last_rom_select);
+
+            let files = Self::rom_select_get_files(rom_select_path).unwrap();
+            let files = ModelRc::from(Rc::new(VecModel::from(
+                files.iter().map(|s| s.into()).collect::<Vec<_>>(),
+            )));
+            Backend::get(&state_.borrow().root.unwrap()).set_rom_select_list(files);
+        });
+
+        let state_ = state.clone();
+        backend.on_rom_select_selected(move |index| {
+            let mut state = state_.borrow_mut();
+            let list = Backend::get(&state.root.unwrap()).get_rom_select_list();
+            if let Some(data) = list.row_data(index as usize) {
+                let path = rom_select_path.join(data.as_str());
+                log::info!("Selected ROM {}", path.display());
+                kvs::keys::LAST_ROM_PATH.set(&path);
+
+                match state.gameboy.set_emulated_cartridge(path.as_path()) {
+                    Ok(_) => true,
+                    Err(e) => {
+                        // TODO show an error message
+                        log::error!("Error loading ROM: {:?}", e);
+                        false
+                    }
+                }
+            } else {
+                false
+            }
+        });
+
+        let state_ = state.clone();
+        backend.on_game_set_paused(move |paused| {
+            let mut state = state_.borrow_mut();
+            state.gameboy.set_paused(paused).unwrap();
+            if paused {
+                // TODO handle error more gracefully
+                state.gameboy.persist_ram().unwrap();
+            }
+        });
+
+        let state_ = state.clone();
+        backend.on_game_reset(move || {
+            state_.borrow_mut().gameboy.reset().unwrap();
+        });
+
+        backend.on_screen_enter(|screen| {
+            // Called when a new screen is entered, before the new frame is rendered.
+            log::info!("Screen enter: {:?}", screen);
+        });
+
+        backend.on_volume_changed({
+            let state_ = state.clone();
+            let timer = Timer::default();
+            move |value| {
+                let volume = ((value * (u8::MAX as i32)) / 100) as u8;
+                Device::lock().dac.set_volume(volume).unwrap();
+                // Start the timer to hide the bar.
+                let state_ = state_.clone();
+                timer.start(
+                    TimerMode::SingleShot,
+                    Duration::from_millis(1000),
+                    move || {
+                        kvs::keys::VOLUME.set(&volume);
+                        Backend::get(&state_.borrow().root.unwrap()).set_volume_visible(false);
+                    },
+                )
+            }
+        });
+
+        backend.on_brightness_changed({
+            let state_ = state.clone();
+            let timer = Timer::default();
+            move |value| {
+                let brightness = (value as f32) / 100.0;
+                Device::lock().set_brightness(brightness);
+                // Start the timer to hide the bar.
+                let state_ = state_.clone();
+                timer.start(
+                    TimerMode::SingleShot,
+                    Duration::from_millis(1000),
+                    move || {
+                        kvs::keys::BRIGHTNESS.set(&brightness);
+                        Backend::get(&state_.borrow().root.unwrap()).set_brightness_visible(false);
+                    },
+                )
+            }
+        });
+
+        backend.on_power_off(|| {
+            Device::lock().power_off();
+        });
+
+        backend.on_settings_updated(|a, b| {
+            log::info!("settings updated: {:?}, {:?}", a, b);
+        });
+
+        // Focus stack: allowing dialogs to push and pop focus.
+        // Uses private, unstable APIs.
+        let state_ = state.clone();
+        backend.on_push_focus(move || {
+            let mut state = state_.borrow_mut();
+            let root = state.root.unwrap();
+            let window_inner = slint_re_exports::WindowInner::from_pub(root.window());
+            let item = window_inner.focus_item.borrow();
+            state.focus_stack.push(item.clone());
+        });
+        let state_ = state.clone();
+        backend.on_pop_focus(move || {
+            let mut state = state_.borrow_mut();
+            let root = state.root.unwrap();
+            let window_inner = slint_re_exports::WindowInner::from_pub(root.window());
+            while let Some(item) = state.focus_stack.pop() {
+                if let Some(item) = item.upgrade() {
+                    window_inner.set_focus_item(&item);
+                    break;
+                }
+            }
+        });
+    }
+}
