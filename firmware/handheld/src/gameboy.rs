bef6f6afadc53bd339734c51300b0717fe7bde56
Eli Lipsitz <eli.lipsitz@gmail.com>
2024-03-31 19:08:08 -0400

    Support loading/saving RTC to save file

--- firmware/handheld/src/gameboy.rs
+++ firmware/handheld/src/gameboy.rs
@@ -13,6 +13,8 @@ const REG_EMU_CART_ROM_ADDR: u32 = 0xC000_0004;
 const REG_EMU_CART_ROM_MASK: u32 = 0xC000_0008;
 const REG_EMU_CART_RAM_ADDR: u32 = 0xC000_000C;
 const REG_EMU_CART_RAM_MASK: u32 = 0xC000_0010;
+const REG_RTC_STATE: u32 = 0xC000_0014;
+const REG_RTC_LATCHED: u32 = 0xC000_0018;
 
 #[derive(Debug, Error)]
 pub enum GameboyError {
@@ -199,14 +201,39 @@ impl Gameboy {
             Ok(mut ram_file) => {
                 log::info!("Loading RAM");
 
-                let mut i = 0u32;
-                loop {
-                    let n = ram_file.read(&mut buf)?;
+                let mut pos = 0u32;
+                while pos < rom_header.ram_size {
+                    let to_read = ((rom_header.ram_size - pos) as usize).min(CHUNK_SIZE);
+                    let n = ram_file.read(&mut buf[..to_read])?;
                     if n == 0 {
                         break;
                     }
-                    device.fpga.sram_write(i, &buf[..n])?;
-                    i += n as u32;
+                    device.fpga.sram_write(pos, &buf[..n])?;
+                    pos += n as u32;
+                }
+
+                if rom_header.has_rtc {
+                    // Read next 48 bytes for RTC data.
+                    let n = ram_file.read(&mut buf[..48])?;
+                    if n == 48 {
+                        let mut rtc_state = RtcState::from_disk(&buf[0..20].try_into().unwrap());
+                        let rtc_latched = RtcState::from_disk(&buf[20..40].try_into().unwrap());
+                        let rtc_timestamp = u64::from_le_bytes(buf[40..48].try_into().unwrap());
+                        let elapsed = device
+                            .get_datetime()
+                            .unix_timestamp()
+                            .saturating_sub_unsigned(rtc_timestamp);
+                        rtc_state.advance(elapsed as u64);
+                        device.fpga.write_u32(REG_RTC_STATE, rtc_state.to_fpga())?;
+                        device
+                            .fpga
+                            .write_u32(REG_RTC_LATCHED, rtc_latched.to_fpga())?;
+                        log::info!(
+                            "Loaded saved RTC state: {:?}, elapsed={}",
+                            rtc_state,
+                            elapsed
+                        );
+                    }
                 }
             }
             Err(_) => {
@@ -226,6 +253,11 @@ impl Gameboy {
         device
             .fpga
             .write_u32(REG_EMU_CART_RAM_MASK, rom_header.ram_size - 1)?;
+
+        log::info!(
+            "debug: {:?}",
+            RtcState::from_fpga(device.fpga.read_u32(REG_RTC_STATE)?)
+        );
 
         // Resume
         device.fpga.write_u32(REG_CONTROL, 0b1011)?;
@@ -261,6 +293,107 @@ impl Gameboy {
             bytes_left -= to_read;
         }
 
+        if self.rom_header.as_ref().map_or(false, |h| h.has_rtc) {
+            let rtc_state = RtcState::from_fpga(device.fpga.read_u32(REG_RTC_STATE)?);
+            let rtc_latched = RtcState::from_fpga(device.fpga.read_u32(REG_RTC_LATCHED)?);
+            file.write(&rtc_state.to_disk())?;
+            file.write(&rtc_latched.to_disk())?;
+            file.write(&(device.get_datetime().unix_timestamp() as u64).to_le_bytes())?;
+            log::info!("Wrote RTC state: {:?}", rtc_state);
+        }
+
         Ok(())
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+struct RtcState {
+    seconds: u8,
+    minutes: u8,
+    hours: u8,
+    days: u16,
+    days_overflow: bool,
+    halt: bool,
+}
+
+impl RtcState {
+    pub fn from_fpga(data: u32) -> Self {
+        // FPGA format: hodddddddddhhhhhmmmmmmssssss
+        RtcState {
+            seconds: ((data >> 0) & 0b111111) as u8,
+            minutes: ((data >> 6) & 0b111111) as u8,
+            hours: ((data >> 12) & 0b11111) as u8,
+            days: ((data >> 17) & 0b111111111) as u16,
+            days_overflow: ((data >> 26) & 1) == 1,
+            halt: ((data >> 27) & 1) == 1,
+        }
+    }
+
+    pub fn to_fpga(&self) -> u32 {
+        (((self.seconds as u32) & 0b111111) << 0)
+            | (((self.minutes as u32) & 0b111111) << 6)
+            | (((self.hours as u32) & 0b11111) << 12)
+            | (((self.days as u32) & 0b111111111) << 17)
+            | ((self.days_overflow as u32) << 26)
+            | ((self.halt as u32) << 27)
+    }
+
+    pub fn from_disk(data: &[u8; 20]) -> Self {
+        let words = [
+            u32::from_le_bytes(data[0..4].try_into().unwrap()),
+            u32::from_le_bytes(data[4..8].try_into().unwrap()),
+            u32::from_le_bytes(data[8..12].try_into().unwrap()),
+            u32::from_le_bytes(data[12..16].try_into().unwrap()),
+            u32::from_le_bytes(data[16..20].try_into().unwrap()),
+        ];
+        RtcState {
+            seconds: (words[0] & 0b111111) as u8,
+            minutes: (words[1] & 0b111111) as u8,
+            hours: (words[2] & 0b11111) as u8,
+            days: ((words[3] & 0xFF) | ((words[4] & 1) << 8)) as u16,
+            halt: ((words[4] >> 6) & 1) == 1,
+            days_overflow: ((words[4] >> 7) & 1) == 1,
+        }
+    }
+
+    pub fn to_disk(&self) -> [u8; 20] {
+        let mut data = [0u8; 20];
+        data[0..4].copy_from_slice(&u32::to_le_bytes(self.seconds as u32));
+        data[4..8].copy_from_slice(&u32::to_le_bytes(self.minutes as u32));
+        data[8..12].copy_from_slice(&u32::to_le_bytes(self.hours as u32));
+        data[12..16].copy_from_slice(&u32::to_le_bytes((self.days & 0xFF) as u32));
+        let last = ((self.days as u32 & 0x100) >> 8)
+            | ((self.halt as u32) << 6)
+            | ((self.days_overflow as u32) << 7);
+        data[16..20].copy_from_slice(&u32::to_le_bytes(last));
+        data
+    }
+
+    fn compute_ticks(value: u16, ticks: &mut u64, wrap_point: u16, max_value: u16) -> u64 {
+        let mut value = value as u64;
+        if value >= (wrap_point as u64) {
+            let needed = (max_value as u64) - value;
+            if *ticks >= needed {
+                *ticks -= needed;
+                value = 0;
+            } else {
+                value += *ticks;
+                *ticks = 0;
+            }
+        }
+        value += *ticks;
+        *ticks = value / (wrap_point as u64);
+        value % (wrap_point as u64)
+    }
+
+    pub fn advance(&mut self, seconds: u64) {
+        let mut ticks = seconds;
+        self.seconds = Self::compute_ticks(self.seconds as u16, &mut ticks, 60, 1 << 6) as u8;
+        self.minutes = Self::compute_ticks(self.minutes as u16, &mut ticks, 60, 1 << 6) as u8;
+        self.hours = Self::compute_ticks(self.hours as u16, &mut ticks, 24, 1 << 5) as u8;
+        self.days = Self::compute_ticks(self.days as u16, &mut ticks, 1 << 9, 1 << 9) as u16;
+        if ticks > 0 {
+            self.days_overflow = true;
+        }
     }
 }
