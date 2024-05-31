8ee40e805ba67a1c3b52c913001a1b3ba738d54d
Eli Lipsitz <eli.lipsitz@gmail.com>
2024-05-30 20:37:07 -0400

    gba: Handle oam write mask and addresses

--- fpga/src/main/scala/gba/ppu/PpuMem.scala
+++ fpga/src/main/scala/gba/ppu/PpuMem.scala
@@ -26,8 +26,9 @@ class PpuMem(size: Int, width: Width) extends Module {
     /// Target interface for the PPU
     val ppuTarget = new PpuMemoryInterface(size, width)
   })
+  val widthHalfwords = width.get / 16
 
-  val mem = SyncReadMem(size, UInt(width))
+  val mem = SyncReadMem(size, Vec(widthHalfwords, UInt(16.W)))
 
   // CPU access
   {
@@ -36,10 +37,17 @@ class PpuMem(size: Int, width: Width) extends Module {
     // Write
     val queuedWrite = RegInit(false.B)
     val queuedWriteAddress = Reg(UInt((log2Ceil(size) + 2).W))
+    val queuedWriteMask = Reg(UInt((width.get / 8).W))
     when (io.enable && queuedWrite) {
-      // TODO support halfword strobe if width > halfword (OAM)
-      // OAM also apparently doesn't support 8 bit writes? TODO: see what happens
-      mem.write(queuedWriteAddress >> 1, io.memTarget.dataWrite)
+      val mask = if (width == 32.W) {
+        // TODO verify this is the correct 8-bit write behavior for OAM
+        Seq(queuedWriteMask(3) || queuedWriteMask(2), queuedWriteMask(1) || queuedWriteMask(0))
+      } else {
+        Seq(true.B)
+      }
+
+      val shift = log2Ceil(width.get / 8)
+      mem.write(queuedWriteAddress >> shift, io.memTarget.dataWrite.asTypeOf(Vec(widthHalfwords, UInt(16.W))), mask)
       queuedWrite := false.B
       io.memTarget.done := true.B
 
@@ -48,6 +56,7 @@ class PpuMem(size: Int, width: Width) extends Module {
     when (io.enable && io.memTarget.request && io.memTarget.write) {
       queuedWrite := true.B
       queuedWriteAddress := io.memTarget.address
+      queuedWriteMask := io.memTarget.mask
     }
 
     // Read
@@ -65,6 +74,6 @@ class PpuMem(size: Int, width: Width) extends Module {
 
   // PPU access
   // TODO: make sure there's a single port -- (or maybe one write port and one read port)
-  io.ppuTarget.readData := mem.read(io.ppuTarget.address, io.ppuTarget.read)
+  io.ppuTarget.readData := mem.read(io.ppuTarget.address, io.ppuTarget.read).asUInt
 }
 
