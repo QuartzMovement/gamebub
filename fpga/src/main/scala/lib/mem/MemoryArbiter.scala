f86a88a40d38e8d0cbe0f8b7ef29bfb6b23c9da6
Eli Lipsitz <eli.lipsitz@gmail.com>
2024-07-26 20:38:10 -0400

    fpga: Improve MemoryArbiter, reduce combinatorial paths

--- fpga/src/main/scala/lib/mem/MemoryArbiter.scala
+++ fpga/src/main/scala/lib/mem/MemoryArbiter.scala
@@ -13,71 +13,49 @@ class MemoryArbiter(addressWidth: Int, dataWidth: Int, n: Int) extends Module {
     val initiator = Vec(n, new MemoryInterface(addressWidth, dataWidth))
   })
 
-  /** Whether we're currently waiting for an access. */
-  val busy = RegInit(false.B)
-  /** If busy, the index of the initiator who initiated the access. */
-  val busyOwner = Reg(UInt(log2Ceil(n).W))
+  io.target.enable := false.B
+  io.target.address := DontCare
+  io.target.write := DontCare
+  io.target.dataWrite := DontCare
+  io.target.writeStrobe := DontCare
 
-  for (i <- 0 until n) {
-    io.initiator(i).done := false.B
-    io.initiator(i).dataRead := io.target.dataRead
+  /// Whether we're currently waiting for an access to complete.
+  val busy = RegInit(false.B)
+  /// If busy, the initiator who initiated the access.
+  val busyOwner = Reg(Vec(n, Bool()))
+  /// Vector of which input ports have a memory request.
+  val requestsVec = Wire(Vec(n, Bool()))
+  /// Which input port is chosen for the next request.
+  val requestChosen = Wire(Vec(n, Bool()))
+
+  for ((initiator, i) <- io.initiator.zipWithIndex) {
+    initiator.done := false.B
+    initiator.dataRead := io.target.dataRead
+    requestsVec(i) := initiator.enable
+
+    when ((busy && busyOwner(i)) || (initiator.enable && requestChosen(i))) {
+      io.target.enable := initiator.enable
+      io.target.address := initiator.address
+      io.target.write := initiator.write
+      io.target.dataWrite := initiator.dataWrite
+      io.target.writeStrobe := initiator.writeStrobe
+    }
+
+    when (busy && busyOwner(i)) {
+      initiator.done := io.target.done
+    }
   }
+
+  // Choose the next request.
+  val isRequesting = WireDefault(requestsVec.asUInt.orR)
+  requestChosen := PriorityEncoderOH(requestsVec)
 
   when (busy) {
-    io.target.address := io.initiator(busyOwner).address
-    io.target.enable := io.initiator(busyOwner).enable
-    io.target.write := io.initiator(busyOwner).write
-    io.target.dataWrite := io.initiator(busyOwner).dataWrite
-    io.target.writeStrobe := io.initiator(busyOwner).writeStrobe
-
     when (io.target.done) {
-      io.initiator(busyOwner).done := true.B
       busy := false.B
     }
-  } .otherwise {
-    io.target.address := DontCare
-    io.target.enable := false.B
-    io.target.write := false.B
-    io.target.dataWrite := DontCare
-    io.target.writeStrobe := DontCare
-
-    val hasRequest = WireDefault(false.B)
-    val chosen = WireDefault(0.U(log2Ceil(n).W))
-
-    for (i <- n - 1 to 0 by -1) {
-      when (io.initiator(i).enable) {
-        chosen := i.asUInt
-        hasRequest := true.B
-
-        io.target.address := io.initiator(i).address
-        io.target.enable := true.B
-        io.target.write := io.initiator(i).write
-        io.target.dataWrite := io.initiator(i).dataWrite
-        io.target.writeStrobe := io.initiator(i).writeStrobe
-      }
-    }
-
-    when (hasRequest) {
-      when (io.target.done) {
-        io.initiator(chosen).done := true.B
-      } .otherwise {
-        busy := true.B
-        busyOwner := chosen
-      }
-    }
+  } .elsewhen (isRequesting) {
+    busyOwner := requestChosen
+    busy := true.B
   }
-
-  /*
-  Start not busy.
-
-  If not busy,
-    find the highest priority initiator requesting access
-      If none, do nothing
-      Pass that access on to the target,
-        and if (by the next cycle), the target is not "done", mark ourselves as busy
-
-  If busy and the target is "done",
-    mark the initiator that started as "done".
-    and set ourselves to not busy
-  */
 }
