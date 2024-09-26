732fac24716ee4aeb57d59d38c230ac15aa99112
Eli Lipsitz <eli.lipsitz@gmail.com>
2024-09-25 21:53:05 -0400

    firmware: Name interrupt thread and give it a larger stack

--- firmware/handheld/src/device/interrupt.rs
+++ firmware/handheld/src/device/interrupt.rs
@@ -43,119 +43,123 @@ impl Device<'_> {
     /// * Volume up, volume down, home, and power buttons
     /// * Shared MCU_IRQ line
     pub(super) fn setup_interrupts() {
-        // Setup interrupts (testing).
-        std::thread::spawn(|| {
-            let notification = Notification::new();
+        // Setup interrupt handler thread.
+        std::thread::Builder::new()
+            .name("Interrupt".to_string())
+            .stack_size(8 * 1024)
+            .spawn(|| {
+                let notification = Notification::new();
 
-            {
-                let device = &mut Device::get().lock().unwrap();
-                setup_gpio_interrupt(
-                    &mut device.button_home,
-                    InterruptType::AnyEdge,
-                    notification.notifier(),
-                    FLAG_HOME,
-                )
-                .unwrap();
-                setup_gpio_interrupt(
-                    &mut device.button_power,
-                    InterruptType::AnyEdge,
-                    notification.notifier(),
-                    FLAG_POWER,
-                )
-                .unwrap();
-                setup_gpio_interrupt(
-                    &mut device.button_vol_up,
-                    InterruptType::AnyEdge,
-                    notification.notifier(),
-                    FLAG_VOL_UP,
-                )
-                .unwrap();
-                setup_gpio_interrupt(
-                    &mut device.button_vol_down,
-                    InterruptType::AnyEdge,
-                    notification.notifier(),
-                    FLAG_VOL_DOWN,
-                )
-                .unwrap();
-                setup_gpio_interrupt(
-                    &mut device.pin_irq,
-                    InterruptType::LowLevel,
-                    notification.notifier(),
-                    FLAG_MCU_IRQ,
-                )
-                .unwrap();
-            }
-
-            let mut prev_hdmi_detected: Option<bool> = None;
-
-            loop {
-                let flags = match notification.wait(esp_idf_svc::hal::delay::BLOCK) {
-                    Some(flags) => flags.get(),
-                    _ => continue,
-                };
-
-                let mut device = Device::get().lock().unwrap();
-
-                if (flags & FLAG_HOME.get()) != 0 {
-                    let _ = device.button_home.enable_interrupt();
-                }
-                if (flags & FLAG_POWER.get()) != 0 {
-                    let _ = device.button_power.enable_interrupt();
-                }
-                if (flags & FLAG_VOL_UP.get()) != 0 {
-                    let _ = device.button_vol_up.enable_interrupt();
-                }
-                if (flags & FLAG_VOL_DOWN.get()) != 0 {
-                    let _ = device.button_vol_down.enable_interrupt();
+                {
+                    let device = &mut Device::get().lock().unwrap();
+                    setup_gpio_interrupt(
+                        &mut device.button_home,
+                        InterruptType::AnyEdge,
+                        notification.notifier(),
+                        FLAG_HOME,
+                    )
+                    .unwrap();
+                    setup_gpio_interrupt(
+                        &mut device.button_power,
+                        InterruptType::AnyEdge,
+                        notification.notifier(),
+                        FLAG_POWER,
+                    )
+                    .unwrap();
+                    setup_gpio_interrupt(
+                        &mut device.button_vol_up,
+                        InterruptType::AnyEdge,
+                        notification.notifier(),
+                        FLAG_VOL_UP,
+                    )
+                    .unwrap();
+                    setup_gpio_interrupt(
+                        &mut device.button_vol_down,
+                        InterruptType::AnyEdge,
+                        notification.notifier(),
+                        FLAG_VOL_DOWN,
+                    )
+                    .unwrap();
+                    setup_gpio_interrupt(
+                        &mut device.pin_irq,
+                        InterruptType::LowLevel,
+                        notification.notifier(),
+                        FLAG_MCU_IRQ,
+                    )
+                    .unwrap();
                 }
 
-                let io_expander = device.io_expander.get_pins().unwrap();
-                let buttons = device.read_button_state(io_expander).unwrap();
-                ui::send(ui::Message::Button(buttons));
+                let mut prev_hdmi_detected: Option<bool> = None;
 
-                let hdmi_detected = device.parse_hdmi_detect(io_expander).unwrap();
-                if prev_hdmi_detected != Some(hdmi_detected) {
-                    prev_hdmi_detected = Some(hdmi_detected);
-                    worker::send(worker::Message::HdmiDetectState(hdmi_detected));
-                }
+                loop {
+                    let flags = match notification.wait(esp_idf_svc::hal::delay::BLOCK) {
+                        Some(flags) => flags.get(),
+                        _ => continue,
+                    };
 
-                if (flags & FLAG_MCU_IRQ.get()) != 0 {
-                    log::debug!("Interrupt: MCU_IRQ");
-                    // N.B. important to read buttons above to clear i/o expander interrupt
+                    let mut device = Device::get().lock().unwrap();
 
-                    // TODO handle other possible interrupt sources, including FPGA
+                    if (flags & FLAG_HOME.get()) != 0 {
+                        let _ = device.button_home.enable_interrupt();
+                    }
+                    if (flags & FLAG_POWER.get()) != 0 {
+                        let _ = device.button_power.enable_interrupt();
+                    }
+                    if (flags & FLAG_VOL_UP.get()) != 0 {
+                        let _ = device.button_vol_up.enable_interrupt();
+                    }
+                    if (flags & FLAG_VOL_DOWN.get()) != 0 {
+                        let _ = device.button_vol_down.enable_interrupt();
+                    }
 
-                    // Fuel gauge IRQs.
-                    if let Ok(fuel_irq) = device.fuel_gauge.query_alerts() {
-                        for (alert, active) in fuel_irq.into_iter() {
-                            if active {
-                                worker::send(worker::Message::FuelGaugeAlert(alert));
+                    let io_expander = device.io_expander.get_pins().unwrap();
+                    let buttons = device.read_button_state(io_expander).unwrap();
+                    ui::send(ui::Message::Button(buttons));
+
+                    let hdmi_detected = device.parse_hdmi_detect(io_expander).unwrap();
+                    if prev_hdmi_detected != Some(hdmi_detected) {
+                        prev_hdmi_detected = Some(hdmi_detected);
+                        worker::send(worker::Message::HdmiDetectState(hdmi_detected));
+                    }
+
+                    if (flags & FLAG_MCU_IRQ.get()) != 0 {
+                        log::debug!("Interrupt: MCU_IRQ");
+                        // N.B. important to read buttons above to clear i/o expander interrupt
+
+                        // TODO handle other possible interrupt sources, including FPGA
+
+                        // Fuel gauge IRQs.
+                        if let Ok(fuel_irq) = device.fuel_gauge.query_alerts() {
+                            for (alert, active) in fuel_irq.into_iter() {
+                                if active {
+                                    worker::send(worker::Message::FuelGaugeAlert(alert));
+                                }
                             }
                         }
-                    }
 
-                    // DAC IRQs.
-                    if let Ok(dac_irq) = device.dac.get_interrupt_status() {
-                        if dac_irq.headset_detected {
-                            let has_headphones = device.dac.get_headphones_detected().unwrap();
-                            worker::send(worker::Message::HeadphoneState(has_headphones));
+                        // DAC IRQs.
+                        if let Ok(dac_irq) = device.dac.get_interrupt_status() {
+                            if dac_irq.headset_detected {
+                                let has_headphones = device.dac.get_headphones_detected().unwrap();
+                                worker::send(worker::Message::HeadphoneState(has_headphones));
+                            }
                         }
-                    }
 
-                    // Read FPGA.
-                    // TODO: only read and ack interrupts that we've enabled?
-                    let fpga_irq = device.fpga.read_u32(fpga::REG_IRQ_STATUS).unwrap();
-                    if fpga_irq != 0 {
-                        device
-                            .fpga
-                            .write_u32(fpga::REG_IRQ_STATUS, fpga_irq)
-                            .unwrap();
-                        worker::send(worker::Message::FpgaIrq(fpga_irq));
-                    }
+                        // Read FPGA.
+                        // TODO: only read and ack interrupts that we've enabled?
+                        let fpga_irq = device.fpga.read_u32(fpga::REG_IRQ_STATUS).unwrap();
+                        if fpga_irq != 0 {
+                            device
+                                .fpga
+                                .write_u32(fpga::REG_IRQ_STATUS, fpga_irq)
+                                .unwrap();
+                            worker::send(worker::Message::FpgaIrq(fpga_irq));
+                        }
 
-                    let _ = device.pin_irq.enable_interrupt();
+                        let _ = device.pin_irq.enable_interrupt();
+                    }
                 }
-            }
-        });
+            })
+            .unwrap();
     }
 }
