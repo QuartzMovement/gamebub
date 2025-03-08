0ead56b5b9484599e74192442b6e2d1cd3823ea7
Eli Lipsitz <eli.lipsitz@gmail.com>
2025-03-07 23:31:11 -0500

    firmware: Implement fpga_read command

--- firmware/handheld/src/cli/mod.rs
+++ firmware/handheld/src/cli/mod.rs
@@ -1,6 +1,15 @@
 //! Command line interface
 
-use crate::kvs;
+use base64::{prelude::BASE64_STANDARD, Engine};
+use esp_idf_svc::hal::units::MegaHertz;
+
+use crate::{
+    device::{
+        drivers::fpga::{FpgaSpiWordSize, SpiCommand},
+        Device,
+    },
+    kvs,
+};
 
 /// Start the CLI thread. Called once during system init.
 pub fn start() {
@@ -30,12 +39,22 @@ fn cli_thread() -> ! {
 
         let result = match command {
             "get_hwinfo" => handle_get_hwinfo(args),
+            "fpga_read" => handle_fpga_read(args),
             _ => Err("unknown command".to_string()),
         };
         if let Err(error) = result {
             println!("<error,{error}");
         }
     }
+}
+
+fn get_arg_u32<'a>(arg: Option<&'a str>) -> Result<u32, String> {
+    let arg = arg.ok_or("missing arg")?;
+    let (arg, base) = match arg.strip_prefix("0x") {
+        Some(x) => (x, 16),
+        None => (arg, 10),
+    };
+    u32::from_str_radix(arg, base).map_err(|_| "invalid number".to_string())
 }
 
 /// `>get_hwinfo`: returns `<ok,{serial number},{hardware revision}`
@@ -45,5 +64,38 @@ fn handle_get_hwinfo<'a>(_args: impl Iterator<Item = &'a str>) -> Result<(), Str
         kvs::keys::DEVICE_SERIAL.get().as_deref().unwrap_or(""),
         kvs::keys::DEVICE_REVISION.get().unwrap_or_default(),
     );
+    Ok(())
+}
+
+/// `>fpga_read,<addr>,<word size>,<max clock MHz>,<length>`: returns `<ok,{base64 data}`
+fn handle_fpga_read<'a>(mut args: impl Iterator<Item = &'a str>) -> Result<(), String> {
+    const MAX_READ_LENGTH: u32 = 1024;
+    let address = get_arg_u32(args.next())?;
+    let word_size = get_arg_u32(args.next())?;
+    let max_clock = MegaHertz(get_arg_u32(args.next())?).into();
+    let length = get_arg_u32(args.next())?;
+    if length > MAX_READ_LENGTH {
+        return Err("length too large".to_string());
+    }
+    let word_size = match word_size {
+        8 => FpgaSpiWordSize::Bits8,
+        16 => FpgaSpiWordSize::Bits16,
+        32 => FpgaSpiWordSize::Bits32,
+        64 => FpgaSpiWordSize::Bits64,
+        _ => return Err("invalid word size".to_string()),
+    };
+
+    let mut buffer = vec![0u8; length as usize];
+    let command = SpiCommand {
+        word_size,
+        byte_swap: true,
+        increment_address: true,
+    };
+    Device::lock()
+        .fpga
+        .spi_read(Some(max_clock), command, address, &mut buffer)
+        .map_err(|_| "read error".to_string())?;
+    let output = BASE64_STANDARD.encode(buffer);
+    println!("<ok,{}", output);
     Ok(())
 }
