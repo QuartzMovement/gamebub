2de02f0e591553d0666c4b59e59a2cd1773559a0
Eli Lipsitz <eli.lipsitz@gmail.com>
2026-02-12 20:21:01 -0500

    dfu: Implement flash write and erase

--- firmware/handheld-dfu/src/protocol.rs
+++ firmware/handheld-dfu/src/protocol.rs
@@ -1,7 +1,7 @@
 use core::cell::RefCell;
 
 use embassy_sync::blocking_mutex::CriticalSectionMutex;
-use embedded_storage::nor_flash::ReadNorFlash;
+use embedded_storage::nor_flash::{NorFlash, ReadNorFlash};
 use esp_storage::FlashStorage;
 
 static STATE: CriticalSectionMutex<RefCell<CommandState>> =
@@ -20,6 +20,8 @@ pub struct CommandHeader {
 }
 
 const FLASH_BASE_ADDR: u32 = 0x3C00_0000;
+/// Flash below this offset is write-protected.
+const FLASH_PROTECT_OFFSET: u32 = 0x8_0000;
 
 impl From<[u8; 32]> for CommandHeader {
     fn from(value: [u8; 32]) -> Self {
@@ -80,7 +82,7 @@ impl Protocol {
     pub async fn handle(
         &mut self,
         command: CommandHeader,
-        _read_fn: impl AsyncFnMut(&mut [u8]) -> Result<usize, CommandStatus>,
+        read_fn: impl AsyncFnMut(&mut [u8]) -> Result<usize, CommandStatus>,
         write_fn: impl AsyncFnMut(&[u8]) -> Result<(), CommandStatus>,
     ) -> Result<(), CommandStatus> {
         STATE.lock(|x| {
@@ -93,11 +95,11 @@ impl Protocol {
         });
         let result = match command.cmd_id {
             // FLASH_ERASE
-            0x03 => Err(CommandStatus::UnknownCmd),
+            0x03 => self.command_flash_erase(command).await,
             // READ
             0x84 => self.command_read(command, write_fn).await,
             // WRITE
-            0x05 => Err(CommandStatus::UnknownCmd),
+            0x05 => self.command_write(command, read_fn).await,
             // TODO efuse
             _ => Err(CommandStatus::UnknownCmd),
         };
@@ -115,6 +117,43 @@ impl Protocol {
         result
     }
 
+    async fn command_flash_erase(&mut self, command: CommandHeader) -> Result<(), CommandStatus> {
+        // Validate arguments.
+        if command.cmd_size != 8 {
+            return Err(CommandStatus::InvalidCmdLength);
+        }
+        let address = u32::from_le_bytes(command.args[0..4].try_into().unwrap());
+        let length = u32::from_le_bytes(command.args[4..8].try_into().unwrap());
+        if command.transfer_length != 0 {
+            return Err(CommandStatus::InvalidTransferLength);
+        }
+        let sector_size = FlashStorage::SECTOR_SIZE;
+        if (address % sector_size) != 0 {
+            return Err(CommandStatus::BadAlignment);
+        }
+        if (length % sector_size) != 0 {
+            return Err(CommandStatus::BadAlignment);
+        }
+        if address < FLASH_PROTECT_OFFSET {
+            return Err(CommandStatus::NotPermitted);
+        }
+        if (address + length) as usize > self.flash.capacity() {
+            return Err(CommandStatus::InvalidArg);
+        }
+
+        let mut start = address;
+        let end = address + length;
+        while start < end {
+            self.flash
+                .erase(start, start + sector_size)
+                .map_err(|_| CommandStatus::UnknownError)?;
+            start += sector_size;
+            embassy_futures::yield_now().await;
+        }
+
+        Ok(())
+    }
+
     async fn command_read(
         &mut self,
         command: CommandHeader,
@@ -129,29 +168,81 @@ impl Protocol {
         if command.transfer_length != length {
             return Err(CommandStatus::InvalidArg);
         }
-        if (address % 4) != 0 {
+        if (address % FlashStorage::WORD_SIZE) != 0 {
+            return Err(CommandStatus::BadAlignment);
+        }
+        if (length % FlashStorage::WORD_SIZE) != 0 {
             return Err(CommandStatus::BadAlignment);
         }
         // TODO: support read from RAM?
         if address < FLASH_BASE_ADDR {
             return Err(CommandStatus::InvalidAddress);
         }
-        if (length as usize) > self.flash.capacity() {
-            return Err(CommandStatus::InvalidTransferLength);
-        }
-        if (length % 4) != 0 {
+        let mut offset = address - FLASH_BASE_ADDR;
+        if (offset + length) as usize > self.flash.capacity() {
             return Err(CommandStatus::InvalidTransferLength);
         }
 
         let mut buffer = [0u8; 64];
-        let mut offset = 0;
-        while offset < length {
-            let amount = ((length - offset) as usize).min(buffer.len());
+        let end_address = offset + length;
+        while offset < end_address {
+            let amount = ((end_address - offset) as usize).min(buffer.len());
             let buffer = &mut buffer[0..amount];
             self.flash
                 .read(offset, buffer)
                 .map_err(|_| CommandStatus::UnknownError)?;
             write_fn(buffer).await?;
+            offset += amount as u32;
+        }
+
+        Ok(())
+    }
+
+    async fn command_write(
+        &mut self,
+        command: CommandHeader,
+        mut read_fn: impl AsyncFnMut(&mut [u8]) -> Result<usize, CommandStatus>,
+    ) -> Result<(), CommandStatus> {
+        // Validate arguments.
+        if command.cmd_size != 8 {
+            return Err(CommandStatus::InvalidCmdLength);
+        }
+        let address = u32::from_le_bytes(command.args[0..4].try_into().unwrap());
+        let length = u32::from_le_bytes(command.args[4..8].try_into().unwrap());
+        if command.transfer_length != length {
+            return Err(CommandStatus::InvalidArg);
+        }
+        if (address % FlashStorage::WORD_SIZE) != 0 {
+            return Err(CommandStatus::BadAlignment);
+        }
+        if (length % FlashStorage::WORD_SIZE) != 0 {
+            return Err(CommandStatus::BadAlignment);
+        }
+        // TODO: support read from RAM?
+        if address < FLASH_BASE_ADDR {
+            return Err(CommandStatus::InvalidAddress);
+        }
+        let offset = address - FLASH_BASE_ADDR;
+        if offset < FLASH_PROTECT_OFFSET {
+            return Err(CommandStatus::NotPermitted);
+        }
+        if (offset + length) as usize > self.flash.capacity() {
+            return Err(CommandStatus::InvalidTransferLength);
+        }
+
+        let mut buffer = [0u8; 64];
+        let mut offset = offset;
+        let end_address = offset + length;
+        while offset < end_address {
+            let expected_amount = ((end_address - offset) as usize).min(buffer.len());
+            let amount = read_fn(&mut buffer).await?;
+            if amount != expected_amount {
+                return Err(CommandStatus::InvalidTransferLength);
+            }
+            let bytes = &buffer[0..amount];
+            self.flash
+                .write(offset, bytes)
+                .map_err(|_| CommandStatus::UnknownError)?;
             offset += amount as u32;
         }
 
