4fc9799d226ce121514d49e438cdeabafb7ee179
Eli Lipsitz <eli.lipsitz@gmail.com>
2026-04-19 15:10:06 -0400

    dfu: Add UF2 writing to virtual disk

--- firmware/handheld-dfu/src/virtual_disk.rs
+++ firmware/handheld-dfu/src/virtual_disk.rs
@@ -2,17 +2,48 @@ use alloc::vec;
 use ghostfat::GhostFat;
 use usbd_scsi::BlockDevice as _;
 
-use crate::flash::Flash;
 use crate::usb_class::msc::BlockDevice;
+use crate::{flash::Flash, uf2};
 
 pub const DISK_BLOCK_SIZE: u32 = 512;
 pub const DISK_SIZE: u32 = 64 * 1024 * 1024;
+pub const FLASH_BLOCK_SIZE: u32 = 256;
+pub const FLASH_SIZE: u32 = 16 * 1024 * 1024;
+pub const FLASH_SECTOR_SIZE: u32 = 64 * 1024;
+
+pub const FAMILY_ID: u32 = 0xC47E5767; // ESP32-S3
+
+const FLASH_BASE_ADDR: u32 = 0;
+/// Flash below this offset is write-protected.
+const FLASH_PROTECT_OFFSET: u32 = 0x8_0000;
+
+struct Transfer {
+    blocks_transferred: u32,
+    total_blocks: u32,
+    complete: bool,
+}
 
 pub struct Uf2VirtualDisk {
-    #[allow(unused)]
     flash: &'static Flash,
 
     fat: GhostFat<'static>,
+
+    /// Bitmap for written blocks.
+    block_map: [u8; (FLASH_SIZE / FLASH_BLOCK_SIZE / u8::BITS) as usize],
+
+    /// Bitmap for erased sectors.
+    sector_map: [u8; (FLASH_SIZE / FLASH_SECTOR_SIZE / u8::BITS) as usize],
+
+    /// Current transfer
+    transfer: Option<Transfer>,
+}
+
+fn get_bit(map: &[u8], bit: usize) -> bool {
+    ((map[bit / 8] >> (bit % 8)) & 1) != 0
+}
+
+fn set_bit(map: &mut [u8], bit: usize) {
+    map[bit / 8] |= 1 << (bit % 8);
 }
 
 impl Uf2VirtualDisk {
@@ -42,7 +73,112 @@ impl Uf2VirtualDisk {
         config.num_blocks = DISK_SIZE / DISK_BLOCK_SIZE;
         let fat = ghostfat::GhostFat::new(files.leak(), config);
 
-        Self { flash, fat }
+        Self {
+            flash,
+            fat,
+            block_map: [0u8; _],
+            sector_map: [0u8; _],
+            transfer: None,
+        }
+    }
+
+    fn begin_transfer(&mut self, total_blocks: u32) {
+        log::info!("Beginning UF2 load blocks={total_blocks}");
+        self.transfer = Some(Transfer {
+            blocks_transferred: 0,
+            total_blocks,
+            complete: false,
+        });
+        self.block_map.fill(0);
+        self.sector_map.fill(0);
+    }
+
+    fn handle_uf2_write(&mut self, data: &[u8; 512]) {
+        if self.transfer.as_ref().map(|t| t.complete).unwrap_or(false) {
+            // Already rebooting, ignore.
+            return;
+        }
+
+        // Validate block.
+        let block = match uf2::Uf2Block::from_bytes(data) {
+            Ok(b) => b,
+            Err(_) => {
+                // Ignore non-UF2 block.
+                return;
+            }
+        };
+        if block.family_id() != FAMILY_ID {
+            // Ignore blocks that aren't targeted here.
+            return;
+        }
+        if block.payload().len() != FLASH_BLOCK_SIZE as usize {
+            return;
+        }
+        if block.address() < FLASH_BASE_ADDR || block.address() >= (FLASH_BASE_ADDR + FLASH_SIZE) {
+            return;
+        }
+        if block.address() % FLASH_BLOCK_SIZE != 0 {
+            return;
+        }
+        // TODO check device type extension tag
+
+        // Possibly begin a new transfer.
+        if let Some(transfer) = self.transfer.as_mut() {
+            if transfer.total_blocks != block.total_blocks() {
+                self.begin_transfer(block.total_blocks());
+            }
+        } else {
+            // TODO validate address
+            // TODO validate number of blocks
+            self.begin_transfer(block.total_blocks());
+        }
+
+        let transfer = self.transfer.as_mut().unwrap();
+        if block.block_number() >= transfer.total_blocks {
+            log::warn!("Invalid block number");
+            return;
+        }
+
+        // Check if the block has been transferred already.
+        if get_bit(&self.block_map, block.block_number() as usize) {
+            return;
+        }
+
+        let flash_address = block.address() - FLASH_BASE_ADDR;
+        if flash_address < FLASH_PROTECT_OFFSET {
+            // Do not write over write-protected areas.
+            log::warn!("Refusing write protected block");
+            return;
+        }
+
+        // Erase the sector if needed.
+        let sector_index = flash_address / FLASH_SECTOR_SIZE;
+        if !get_bit(&self.sector_map, sector_index as usize) {
+            let result = self.flash.erase(
+                sector_index * FLASH_SECTOR_SIZE,
+                (sector_index + 1) * FLASH_SECTOR_SIZE,
+            );
+            if let Err(e) = result {
+                log::error!("Flash erase error {:?}", e);
+                return;
+            }
+            set_bit(&mut self.sector_map, sector_index as usize);
+        }
+
+        // Write the payload.
+        let result = self.flash.write(block.address(), block.payload());
+        if let Err(e) = result {
+            log::error!("Flash write error {:?}", e);
+            return;
+        }
+        set_bit(&mut self.block_map, block.block_number() as usize);
+
+        transfer.blocks_transferred += 1;
+        if transfer.blocks_transferred == transfer.total_blocks {
+            log::info!("UF2 complete, rebooting");
+            transfer.complete = true;
+            crate::reboot::reboot();
+        }
     }
 }
 
@@ -61,8 +197,14 @@ impl BlockDevice for Uf2VirtualDisk {
         self.fat.read_block(lba, buf)
     }
 
-    fn write_block(&mut self, _lba: u32, _data: &[u8]) -> Result<(), Self::Error> {
-        Ok(())
+    fn write_block(&mut self, _lba: u32, data: &[u8]) -> Result<(), Self::Error> {
+        match data.try_into() {
+            Ok(x) => {
+                self.handle_uf2_write(x);
+                Ok(())
+            }
+            Err(_) => Ok(()),
+        }
     }
 
     fn flush(&mut self) -> Result<(), Self::Error> {
