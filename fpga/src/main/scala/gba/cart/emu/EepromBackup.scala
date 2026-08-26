QuartzMovement/gamebub

commit 7d47149a2f52bd7607accf401fc958f4a95d7e3a

gba: Fix eeprom (512B) write address calculation

fpga/src/main/scala/gba/cart/emu/EepromBackup.scala
@@ -94,7 +94,8 @@ class EepromBackup extends Module {
         regCounter := nextCounter
         val addressSize = Mux(eepromSize === 0.U, 6.U, 14.U)
         when (nextCounter === addressSize) {
-          regAddress := nextData
+          val address = Mux(eepromSize === 0.U, nextData(5, 0), regData(13, 0))
+          regAddress := address
           regCounter := 0.U
           regState := State.GetWriteData
         }

commit a440ea1852832b9a079849cedaf8497b11cd2768

fpga: Fix GBA eeprom 8KB writes
This was broken in commit 7d47149 (Nov 23, 2024):
> gba: Fix eeprom (512B) write address calculation

Simple typo.

fpga/src/main/scala/gba/cart/emu/EepromBackup.scala
@@ -94,7 +94,8 @@ class EepromBackup extends Module {
         regCounter := nextCounter
         val addressSize = Mux(eepromSize === 0.U, 6.U, 14.U)
         when (nextCounter === addressSize) {
-          val address = Mux(eepromSize === 0.U, nextData(5, 0), regData(13, 0))
+          val address = Mux(eepromSize === 0.U, nextData(5, 0), nextData(13, 0))
+
           regAddress := address
           regCounter := 0.U
           regState := State.GetWriteData